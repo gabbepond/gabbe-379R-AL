28bac105169ce26c24ea19da55a0beb61d1258be
Gabbe Pond <114202309+gabbepond@users.noreply.github.com>
2025-04-28 20:05:18 -0600

    changes

--- src/lib/components/FileUploadAside.svelte
+++ src/lib/components/FileUploadAside.svelte
@@ -9,11 +9,11 @@
 
 </script>
 
-<div class="flex flex-col items-center justify-start m-8">
-    <form class="flex flex-col gap-4" method="POST" action="/chat?/uploadFile" enctype="multipart/form-data" onsubmit={handleSubmit}>
+<div class="flex flex-col items-center justify-start m-4 border-2 border-cyan-600 rounded-lg p-4">
+    <form class="flex flex-col gap-4 " method="POST" action="/chat?/uploadFile" enctype="multipart/form-data" onsubmit={handleSubmit}>
         <h2>Choose The Data File (in PDF format) You Would Like To Upload</h2>
-        <label class="label">
-            <span class="label-text">Select a file</span>
+        <label class="label ">
+            <span class="label-text text-cyan-600">Select a file</span>
             <input class="input" type="file" accept=".pdf" name="file" />
         </label>
         <button class="btn bg-cyan-600 text-white" type="submit">Upload Chosen File</button>
